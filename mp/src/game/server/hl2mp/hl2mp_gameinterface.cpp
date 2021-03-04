589906c2a0003242d0646f36214ede9bd8a701da
9A9A <9A9A@protonmail.com>
2021-03-04 09:09:21 +0300

    Maxplayers changed to 32

--- mp/src/game/server/hl2mp/hl2mp_gameinterface.cpp
+++ mp/src/game/server/hl2mp/hl2mp_gameinterface.cpp
@@ -18,8 +18,8 @@
 
 void CServerGameClients::GetPlayerLimits( int& minplayers, int& maxplayers, int &defaultMaxPlayers ) const
 {
-	minplayers = defaultMaxPlayers = 2; 
-	maxplayers = 16;
+	minplayers = 2;
+	maxplayers = defaultMaxPlayers = MAX_PLAYERS - 1;
 }
 
 // -------------------------------------------------------------------------------------------- //
