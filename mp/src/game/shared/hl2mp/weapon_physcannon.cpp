721ada37e705b3c108815024d0f35c2047b8cbba
speedvoltage <phantomghostdev@pm.me>
2024-05-20 20:29:42 +0200

    Physcannon sprint after detach

--- mp/src/game/shared/hl2mp/weapon_physcannon.cpp
+++ mp/src/game/shared/hl2mp/weapon_physcannon.cpp
@@ -761,10 +761,6 @@ void CPlayerPickupController::Init( CBasePlayer *pPlayer, CBaseEntity *pObject )
 
 
 	CHL2MP_Player *pOwner = (CHL2MP_Player *)ToBasePlayer( pPlayer );
-	if ( pOwner )
-	{
-		pOwner->EnableSprint( false );
-	}
 
 	// If the target is debris, convert it to non-debris
 	if ( pObject->GetCollisionGroup() == COLLISION_GROUP_DEBRIS )
