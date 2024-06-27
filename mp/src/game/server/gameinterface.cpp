dc2840efd7e69de1be3c72bb9f9d7b181f739979
speedvoltage <phantomghostdev@pm.me>
2024-06-27 13:17:31 +0200

    Default the tickrate to 100

--- mp/src/game/server/gameinterface.cpp
+++ mp/src/game/server/gameinterface.cpp
@@ -828,23 +828,18 @@ float CServerGameDLL::GetTickInterval( void ) const
 {
 	float tickinterval = DEFAULT_TICK_INTERVAL;
 
-//=============================================================================
-// HPE_BEGIN:
-// [Forrest] For Counter-Strike, set default tick rate of 66 and removed -tickrate command line parameter.
-//=============================================================================
-// Ignoring this for now, server ops are abusing it
-#if !defined( TF_DLL ) && !defined( CSTRIKE_DLL ) && !defined( DOD_DLL )
-//=============================================================================
-// HPE_END
-//=============================================================================
-	// override if tick rate specified in command line
 	if ( CommandLine()->CheckParm( "-tickrate" ) )
 	{
 		float tickrate = CommandLine()->ParmValue( "-tickrate", 0 );
 		if ( tickrate > 10 )
 			tickinterval = 1.0f / tickrate;
 	}
-#endif
+
+	else
+	{
+		float tickrate = 100.0;
+		tickinterval = 1.0f / tickrate;
+	}
 
 	return tickinterval;
 }
