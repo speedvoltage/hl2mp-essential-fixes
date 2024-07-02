184ce81070fa6f7031635e0a3627b8b3e5d59607
speedvoltage <phantomghostdev@pm.me>
2024-07-02 18:45:56 +0200

    More code clean up

--- mp/src/game/server/hl2mp/hl2mp_cvars.cpp
+++ mp/src/game/server/hl2mp/hl2mp_cvars.cpp
@@ -8,22 +8,6 @@
 #include "cbase.h"
 #include "hl2mp_cvars.h"
 
-
-
-// Ready restart
-ConVar mp_readyrestart(
-	"mp_readyrestart",
-	"0",
-	FCVAR_GAMEDLL,
-	"If non-zero, game will restart once each player gives the ready signal");
-
-// Ready signal
-ConVar mp_ready_signal(
-	"mp_ready_signal",
-	"ready",
-	FCVAR_GAMEDLL,
-	"Text that each player must speak for the match to begin");
-
 ConVar mp_noweapons(
 	"mp_noweapons",
 	"0",
