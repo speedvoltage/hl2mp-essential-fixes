e9f44b68c1d5b6a3d9659a4c8892150e847a6b3b
speedvoltage <phantomghostdev@pm.me>
2024-09-30 16:49:05 +0200

    Fix swept box rays with OBBs

--- mp/src/game/server/gameinterface.cpp
+++ mp/src/game/server/gameinterface.cpp
@@ -221,7 +221,7 @@ static ConVar *g_pcv_hideServer = NULL;
 
 ConVar sv_show_client_connect_msg("sv_show_client_connect_msg", "1", 0, "For showing a public message in chat when a player connects.");
 ConVar sv_show_client_disconnect_msg("sv_show_client_disconnect_msg", "1", 0, "For showing a public message in chat when a player disconnects.");
-ConVar sv_binaries_version("sv_binaries_version", "1.0.9", FCVAR_SPONLY, "Current version of the binaries by Peter Brev");
+ConVar sv_binaries_version("sv_version", "1.0.9", FCVAR_SPONLY, "Current version of the binaries by Peter Brev");
 
 // String tables
 INetworkStringTable *g_pStringTableParticleEffectNames = NULL;
