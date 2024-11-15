bd8ed97fd42f818e92d8705c9b5e93a2f7f2968f
speedvoltage <phantomghostdev@pm.me>
2024-11-15 12:57:31 +0100

    Bump version

--- mp/src/game/server/gameinterface.cpp
+++ mp/src/game/server/gameinterface.cpp
@@ -227,7 +227,7 @@ CON_COMMAND( sv_version, "Binaries version" )
 {
 	if ( UTIL_IsCommandIssuedByServerAdmin() )
 	{
-		Msg( "Binaries version: 1.1.2.178\n" ); // May 19 2024
+		Msg( "Binaries version: 1.1.3.179\n" ); // May 19 2024
 		Msg( "Author: Peter Brev\n" );
 		return;
 	}
