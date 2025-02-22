4c9ba514ce75a16851aab99c159f2ae227ef8701
speedvoltage <32344549+speedvoltage@users.noreply.github.com>
2025-02-22 18:25:12 +0100

    Add sv_displaymotd
    ► Added new ConVar sv_displaymotd to allow showing or not of the MOTD after server entry.

--- src/game/server/hl2mp/hl2mp_client.cpp
+++ src/game/server/hl2mp/hl2mp_client.cpp
@@ -34,6 +34,7 @@
 void Host_Say( edict_t *pEdict, bool teamonly );
 
 ConVar sv_motd_unload_on_dismissal( "sv_motd_unload_on_dismissal", "0", 0, "If enabled, the MOTD contents will be unloaded when the player closes the MOTD." );
+ConVar sv_displaymotd( "sv_displaymotd", "1", 0, "If enabled, display the MOTD to players after server entry." );
 
 extern CBaseEntity*	FindPickerEntityClass( CBasePlayer *pPlayer, char *classname );
 extern bool			g_fGameOver;
@@ -63,18 +64,21 @@ void FinishClientPutInServer( CHL2MP_Player *pPlayer )
 		ClientPrint( pPlayer, HUD_PRINTTALK, "You are on team %s1\n", pPlayer->GetTeam()->GetName() );
 	}
 
-	const ConVar *hostname = cvar->FindVar( "hostname" );
-	const char *title = (hostname) ? hostname->GetString() : "MESSAGE OF THE DAY";
+	if ( sv_displaymotd.GetBool() )
+	{
+		const ConVar *hostname = cvar->FindVar( "hostname" );
+		const char *title = ( hostname ) ? hostname->GetString() : "MESSAGE OF THE DAY";
 
-	KeyValues *data = new KeyValues("data");
-	data->SetString( "title", title );		// info panel title
-	data->SetString( "type", "1" );			// show userdata from stringtable entry
-	data->SetString( "msg",	"motd" );		// use this stringtable entry
-	data->SetBool( "unload", sv_motd_unload_on_dismissal.GetBool() );
+		KeyValues *data = new KeyValues( "data" );
+		data->SetString( "title", title );		// info panel title
+		data->SetString( "type", "1" );			// show userdata from stringtable entry
+		data->SetString( "msg", "motd" );		// use this stringtable entry
+		data->SetBool( "unload", sv_motd_unload_on_dismissal.GetBool() );
 
-	pPlayer->ShowViewPortPanel( PANEL_INFO, true, data );
+		pPlayer->ShowViewPortPanel( PANEL_INFO, true, data );
 
-	data->deleteThis();
+		data->deleteThis();
+	}
 }
 
 /*
