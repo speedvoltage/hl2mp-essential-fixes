110ad2474c65a930f1b3a072cd682575cb7f843e
speedvoltage <32344549+speedvoltage@users.noreply.github.com>
2025-02-23 22:43:14 +0100

    Fix ammo crate exploit
    ► Fixed an issue where players could open infinite ammo crate by throwing an explosive element to the crate and switching to the crowbar before it exploded.
    ► Added stunstick as a weapon that can open the crates.

--- src/game/server/hl2/item_ammo.cpp
+++ src/game/server/hl2/item_ammo.cpp
@@ -877,12 +877,18 @@ void CItem_AmmoCrate::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TY
 	if ( pPlayer == NULL )
 		return;
 
+	if ( m_flCloseTime > gpGlobals->curtime )
+		return;
+
+	if ( !pPlayer->IsAlive() )
+		return;
+
 	m_OnUsed.FireOutput( pActivator, this );
 
 	int iSequence = LookupSequence( "Open" );
 
 	// See if we're not opening already
-	if ( GetSequence() != iSequence )
+	if ( GetSequence() != iSequence && pPlayer->IsAlive() )
 	{
 		Vector mins, maxs;
 		trace_t tr;
@@ -929,7 +935,7 @@ int CItem_AmmoCrate::OnTakeDamage( const CTakeDamageInfo &info )
 	{
 		CBaseCombatWeapon *weapon = player->GetActiveWeapon();
 
-		if (weapon && !stricmp(weapon->GetName(), "weapon_crowbar"))
+		if ( weapon && ( ( !stricmp( weapon->GetName(), "weapon_crowbar" ) || !stricmp( weapon->GetName(), "weapon_stunstick" ) ) ) && info.GetDamageType() & DMG_CLUB )
 		{
 			// play the normal use sound
 			player->EmitSound( "HL2Player.Use" );
