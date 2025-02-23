f95ac0cdad818b82125cc1b52f383656b5323096
speedvoltage <32344549+speedvoltage@users.noreply.github.com>
2025-02-23 16:23:12 +0100

    Fix NULL pointers
    ► Fixed potential NULL pointers.

--- src/game/server/hl2/hl2_player.cpp
+++ src/game/server/hl2/hl2_player.cpp
@@ -3800,6 +3800,9 @@ const impactdamagetable_t &CHL2_Player::GetPhysicsImpactDamageTable()
 //-----------------------------------------------------------------------------
 void CHL2_Player::Splash( void )
 {
+	if ( IsObserver() )
+		return;
+
 	CEffectData data;
 	data.m_fFlags = 0;
 	data.m_vOrigin = GetAbsOrigin();
