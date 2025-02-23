9202826a738b48d9b27ec9d0f25737616ec0e633
speedvoltage <32344549+speedvoltage@users.noreply.github.com>
2025-02-23 21:32:45 +0100

    Verify that a vehicle actually exists
    ► If there is no vehicle, simply return NULL.

--- src/game/server/fourwheelvehiclephysics.h
+++ src/game/server/fourwheelvehiclephysics.h
@@ -178,7 +178,15 @@ inline int CFourWheelVehiclePhysics::GetSpeed() const
 
 inline int CFourWheelVehiclePhysics::GetMaxSpeed() const
 {
-	return INS2MPH(m_pVehicle->GetVehicleParams().engine.maxSpeed);
+	if ( m_pVehicle )
+	{
+		return INS2MPH( m_pVehicle->GetVehicleParams().engine.maxSpeed );
+	}
+
+	else
+	{
+		return NULL;
+	}
 }
 
 inline int CFourWheelVehiclePhysics::GetRPM() const
