6519eb34b49242d93488051c34a120cca07254e5
speedvoltage <32344549+speedvoltage@users.noreply.github.com>
2025-02-23 21:19:54 +0100

    Add a delay before firing physcannon again
    ► Added a 0.5 second delay before being able to use primary attack again on the physcannon.

--- src/game/shared/hl2mp/weapon_physcannon.cpp
+++ src/game/shared/hl2mp/weapon_physcannon.cpp
@@ -1466,6 +1466,7 @@ void CWeaponPhysCannon::PrimaryAttack( void )
 			{
 				// We can't punt this yet
 				DryFire();
+				m_flNextPrimaryAttack = gpGlobals->curtime + 0.5f;
 				return;
 			}
 		}
