bed1585e56e72cb86c387c80ce1733f734a07939
speedvoltage <32344549+speedvoltage@users.noreply.github.com>
2025-02-26 12:33:57 +0100

    Don't use NULL for screenentity in MP
    ► Always try to find a player in multiplayer.

--- src/game/server/sceneentity.cpp
+++ src/game/server/sceneentity.cpp
@@ -3862,7 +3862,14 @@ CBaseEntity *CSceneEntity::FindNamedEntity( const char *name, CBaseEntity *pActo
 
 	if ( !stricmp( name, "Player" ) || !stricmp( name, "!player" ))
 	{
-		entity = ( gpGlobals->maxClients == 1 ) ? ( CBaseEntity * )UTIL_GetLocalPlayer() : NULL;
+		if ( pActor )
+		{
+			entity = gEntList.FindEntityGenericNearest( "player", pActor->GetAbsOrigin(), 0, pActor );
+		}
+		else
+		{
+			entity = ( CBaseEntity * ) UTIL_PlayerByIndex( 1 );
+		}
 	}
 	else if ( !stricmp( name, "!target1" ) )
 	{
