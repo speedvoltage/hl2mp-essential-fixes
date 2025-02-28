6c4cec64dda8699b03568e8f68eb1a0c8eb54ad6
speedvoltage <32344549+speedvoltage@users.noreply.github.com>
2025-02-28 20:25:12 +0100

    Fix an issue of x86-64 and ARM64
    ► Fixed computation of remaining 3 bytes of PVS and 2x speedup PVS computation on x86-64 and ARM64.

--- src/game/server/util.cpp
+++ src/game/server/util.cpp
@@ -2155,11 +2155,20 @@ void UTIL_SetClientVisibilityPVS( edict_t *pClient, const unsigned char *pvs, in
 
 		g_CheckClient.m_bClientPVSIsExpanded = false;
 
+		// old, revert if problematic
+		/*
 		unsigned *pFrom = (unsigned *)pvs;
 		unsigned *pMask = (unsigned *)g_CheckClient.m_checkPVS;
 		unsigned *pTo = (unsigned *)g_CheckClient.m_checkVisibilityPVS;
 
 		int limit = pvssize / 4;
+		*/
+
+		size_t *pFrom = ( size_t * ) pvs;
+		size_t *pMask = ( size_t * ) g_CheckClient.m_checkPVS;
+		size_t *pTo = ( size_t * ) g_CheckClient.m_checkVisibilityPVS;
+		int limit = pvssize / static_cast< int >( sizeof( size_t ) );
+
 		int i;
 
 		for ( i = 0; i < limit; i++ )
@@ -2172,7 +2181,9 @@ void UTIL_SetClientVisibilityPVS( edict_t *pClient, const unsigned char *pvs, in
 			}
 		}
 
-		int remainder = pvssize % 4;
+		// int remainder = pvssize % 4;
+		int remainder = pvssize % static_cast< int >( sizeof( size_t ) );
+
 		for ( i = 0; i < remainder; i++ )
 		{
 			((unsigned char *)&pTo[limit])[i] = ((unsigned char *)&pFrom[limit])[i] & !((unsigned char *)&pMask[limit])[i];
